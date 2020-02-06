7146800c46afd909eeb1273552a040086d7c28dc
doubleInc <aalavi@protonmail.com>
2020-02-06 16:18:58 +1100

    "Added logic to game."

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,11 +1,29 @@
+//  @ts-check (for type checking in vscode)
+
 //console.log("Up and running!");
 
 //
+const cards = ["queen", "queen", "king", "king"];
 
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
+// console.log("User flipped " + cardOne);
+// console.log("User flipped " + cardThree);
+
+let cardOne = cards[0];
+let cardTwo = cards[2];
+
+const cardsInPlay = [];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+cardsInPlay;
 
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You have found a match!");
+  } else {
+    alert("Sorry, try again!");
+  }
+}
